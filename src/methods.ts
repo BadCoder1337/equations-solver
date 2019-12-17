a7c40c6e0a913f0f7f9697e1e926fb454a2e1546
BadCoder1337 <anton.tiz.musin@gmail.com>
2019-12-17 09:13:50 +0300

    refactor: update chord method

--- src/methods.ts
+++ src/methods.ts
@@ -39,23 +39,11 @@ export default class Methods {
     public static [SolvingMethod.CHORD]([a, b]: ArrayPoint): number {
         const e = Store.get('eps');
         const F = (xx: number) => objects.evaluatex!({ x: xx });
-        let Fa = F(a);
-        let Fb = F(b);
-        let x = b;
-        const S = Math.sign(Fa);
-        while (b - a > e) {
-            x = a - (b - a) / (Fb - Fa) * Fa;
-            const R = F(x);
-            if (Math.abs(R) < e) { break; }
-            if (Math.sign(R) === S) {
-                a = x;
-                Fa = R;
-            } else {
-                b = x;
-                Fb = R;
-            }
+        while (Math.abs(b - a) > e) {
+            a = b - (b - a) * F(b) / (F(b) - F(a));
+            b = a + (a - b) * F(a) / (F(a) - F(b));
         }
-        return x;
+        return b;
     }
 
     public static derivative(x: number) {
